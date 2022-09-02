68ca92c21783810c57c9874c0e6d3d02febca9a0
qraztec <110706577+qraztec@users.noreply.github.com>
2022-09-02 12:14:33 -0700

    Unit Tests for receipt accuracy

--- PizzaPlace/src/UnitTests.java
+++ PizzaPlace/src/UnitTests.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 class UnitTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testReceipt() {
+		Receipt r = new Receipt(null, 0, null, null, null, 0.05, null);
+		assertEquals(2.5, r.calculateSubTotal(50));
+		assertEquals(1.74, r.calculateSubTotal(34.7));
+		assertEquals(1.80, r.calculateSubTotal(35.99));
+		
 	}
 
 }
